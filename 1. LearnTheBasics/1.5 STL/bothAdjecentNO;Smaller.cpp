dbf9aeaf1addfe50b20b1677395450dd0c51ef06
ASHUTOSH KUSHWAHA <133047202+blueewhitee@users.noreply.github.com>
2024-01-03 01:43:51 +0530

    better sol

--- 1. LearnTheBasics/1.5 STL/bothAdjecentNO;Smaller.cpp
+++ 1. LearnTheBasics/1.5 STL/bothAdjecentNO;Smaller.cpp
@@ -1,5 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+vector<int> functions2(vector<int>sol){
+    vector<int>temp;
+    int i = 1;
+    for(auto p = sol.begin()+1;p!=sol.end()-1;p++){
+        if(sol[i-1]>*(p) && sol[i+1]>*(p)){
+            temp.push_back(*(p));
+        }
+        i++;
+    }
+    return temp;
+
+}
+int main(){
+    vector<int>num = {6,2,5,0,4,3,7,7,5,6};
+  vector<int>result = functions2(num);
+  for(auto it : result){
+    cout<<it;
+  }
+return 0;
+}-------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include<bits/stdc++.h>
+using namespace std;
 void functions(vector<int>sol){
     int i=0;
     vector<int>temp;

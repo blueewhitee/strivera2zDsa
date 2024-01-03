cb8731f03f6f36f7a9e713305c6ff80eab632f26
ASHUTOSH KUSHWAHA <133047202+blueewhitee@users.noreply.github.com>
2024-01-03 10:05:35 +0530

    Create simpleMatrrix.cpp

---
+++ 1. LearnTheBasics/1.5 STL/simpleMatrrix.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+void functions2(int n){
+  vector<int>matrix(n,n);
+  for(auto it: matrix){
+    for(auto it2 : matrix){
+      cout<<n;
+    }
+    cout<<endl;
+  }
+}
+int main(){
+  int n;
+  cin>>n;
+  functions2(n);
+  return 0;
+}
+

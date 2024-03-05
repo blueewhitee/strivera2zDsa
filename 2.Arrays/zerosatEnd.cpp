b449f2408f1639eea3d4b58eda4956b68398a958
blueewhitee <ak2039@srmist.edu.in>
2024-03-05 22:21:36 +0530

    commit

--- 2.Arrays/zerosatEnd.cpp
+++ 2.Arrays/zerosatEnd.cpp
@@ -1,28 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
     void funcc(vector<int>& nums,int n ) {
-        vector<int>nums2;
         int i{0};
         int j{0};
-        while(i<n){
-            if(nums[i]!=0){
-                nums2.push_back(nums[i]);
+        while(j<n){
+            if(nums[j]==0){
                 j++;
             }
-            i++;
-        }
-        //int zeroes = n - nums2.size();
-        while(j<n){
+            if (nums[i]!=0){
+                i++;
+            }
+            if(j>i&&nums[j]!=0&&nums[i]==0){
+                swap(nums[j],nums[i]);
+                i++;
+                j++;
+            }
             j++;
-            nums2.push_back(0);
-        }
-        for(auto it: nums2){
-            cout<<it<<'\n';
+
         }
 
-
+        for(auto it: nums){
+            cout<<it<<'\n';}
 
     }
+/*        int n  = nums.size();
+        if(n==2){
+            if(nums[0]==0 &&nums[1]!=0){
+                nums[0]=nums[1];
+                nums[1]=0;
+                }
+            
+        }
+
+        if(n>2){
+        int i{0};
+        int j {0};
+        while(j<n){
+            if(nums[i]!=0 ){
+                i++;
+            
+            }
+            if(nums[j]==0){
+                j++;
+            }
+            if (nums[j]!=0 && nums[i]==0 && i<j){
+                nums[i]=nums[j];
+                nums[j]=0;
+                
+                
+              //  i=j;
+            }
+            j++;
+            
+            
+            
+        }*/
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
 /*  BRUTE FORCE APPROACH-  
     vector<int>nums2;
         int i{0};
@@ -50,7 +85,7 @@ using namespace std;
 
 int main(){
     vector<int >nums;
-    nums = {0,1,0,3,12};
+    nums = {1 ,0 ,2 ,3 ,0 ,4 ,0 ,1};
     int n  =  nums.size();
 
     funcc(nums,n);
